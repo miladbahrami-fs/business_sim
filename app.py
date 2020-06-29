3bb2071d3ca3e33d543bdbb9f1029c6d193ab3ed
miladbahrami-fs <54926842+miladbahrami-fs@users.noreply.github.com>
2020-06-29 21:32:35 +0430

    Update app.py
    - Adding summary section
    - Adding lables for inputs

--- app.py
+++ app.py
@@ -44,10 +44,34 @@ app.layout = html.Div([
 
     html.Div(id='parameter-state',children=[
         html.H3('''Please inter your campaing parameters for simulation.'''),
-        dcc.Input(id='spend-inp', type="number", value=1000,placeholder='Spend amount'),
-        dcc.Input(id='ctl-inp', type="number", value=0.2, placeholder='Click to lead ratio'),
-        dcc.Input(id='clv-inp',type='number',value=58,placeholder='Expected CLV'),
-        html.Button(id='submit-button-state', n_clicks=0, children='Simulate')]),
+        html.Label('Spend Amount'),
+        dcc.Input(
+            id='spend-inp',
+            type="number",
+            value=1000,
+            placeholder='Spend amount'),
+        html.Label('Cost Per Click'),
+        dcc.Input(
+            id='cpc-inp',
+            type='number',
+            value=1.75,
+            placeholder='Cost Per Click'),
+        html.Label('Click To Lead'),
+        dcc.Input(
+            id='ctl-inp',
+            type="number",
+            value=0.2,
+            placeholder='Click to lead ratio'),
+        html.Label('CLV'),
+        dcc.Input(
+            id='clv-inp',
+            type='number',
+            value=58,
+            placeholder='Expected CLV'),
+        html.Button(
+            id='submit-button-state',
+            n_clicks=0,
+            children='Simulate')]),
 
     html.Div(id='output-state',
     children=[
@@ -56,8 +80,20 @@ app.layout = html.Div([
     dcc.Graph(id='clv-cpa'),
     dcc.Graph(id='new-customer'),
     dcc.Graph(id='campaign-ltv'),
-    dcc.Graph(id='campaign-return')])
+    dcc.Graph(id='campaign-return')]),
+    
+    html.Div(id='summary-state',children=[
+        html.H2('Simulation Summary'),
+        html.Table([
+            html.Tr([html.Td('Expected Customer Life Time Value'), html.Td(id='summary-clv')]),
+            html.Tr([html.Td('Mean Cost Per Acquisition'), html.Td(id='summary-cpa')]),
+            html.Tr([html.Td('Mean CLV - CPA'), html.Td(id='summary-clv-cpa')]),
+            html.Tr([html.Td('Mean Number of New Customers'), html.Td(id='summary-new-customer')]),
+            html.Tr([html.Td('Mean Campaign LTV'), html.Td(id='summary-ltv')]),
+            html.Tr([html.Td('Mean Campaign Return'), html.Td(id='summary-return')])
+            ])
     ])
+])
 
 @app.callback(
     [Output('clv', 'figure'),
@@ -65,15 +101,24 @@ app.layout = html.Div([
     Output('clv-cpa','figure'),
     Output('new-customer','figure'),
     Output('campaign-ltv','figure'),
-    Output('campaign-return','figure')],
+    Output('campaign-return','figure'),
+    Output('summary-clv','children'),
+    Output('summary-cpa','children'),
+    Output('summary-clv-cpa','children'),
+    Output('summary-new-customer','children'),
+    Output('summary-ltv','children'),
+    Output('summary-return','children')],
     [Input('submit-button-state','n_clicks')],
-    [State('spend-inp', 'value'), State('ctl-inp', 'value'),State('clv-inp','value')])
-def update_figure(n_clicks,spend, ctl, expected_clv):
+    [State('spend-inp', 'value'),
+    State('cpc-inp','value'),
+    State('ctl-inp', 'value'),
+    State('clv-inp','value')])
+def update_figure(n_clicks, spend, cpc, ctl, expected_clv):
     # variable
     # we can discussed how we define each variables here
 
     spend=spend
-    cpc=1.73
+    cpc=cpc
 
     # Conversion rate, depends on we look at cpc or cpm
     # here i use cpc, and hence the conversion rate is click > lead(virtual) > real , as we dont really care roi of camp for BA (which use cpm)
@@ -106,7 +151,7 @@ def update_figure(n_clicks,spend, ctl, expected_clv):
     clv_list=[]
     campaign_ltv_list=[]
 
-    for i in range(1000):
+    for _ in range(1000):
 
         # Run marketing campaign sim
         conversion_rate=get_conversion_rate(conversion_rate_expected,
@@ -132,15 +177,11 @@ def update_figure(n_clicks,spend, ctl, expected_clv):
     results_df['campaign_ltv']=campaign_ltv_list
     results_df['campaign_return']=np.array(campaign_ltv_list) - spend
 
-    # columns=results_df[['new_customer_count', 'cpa',
-    #     'campaign_return', 'clv - cpa']].columns
-
     
     # fig=ff.create_distplot([results_df[c] for c in columns], columns)
     # fig = make_subplots(rows=3, cols=1)
     # fig.add_trace(ff.create_distplot([results_df['clv']],['CLVDist']),row=1,col=1)
     # fig.add_trace(ff.create_distplot([results_df['cpa']],['CPADist']),row=2,col=1)
-    # Add title
     fig_clv=ff.create_distplot([results_df['clv']],['CLV'])
     fig_clv.update_layout(title_text='Customer Life Time Value')
     
@@ -158,7 +199,14 @@ def update_figure(n_clicks,spend, ctl, expected_clv):
 
     fig_campaign_return=ff.create_distplot([results_df['campaign_return']],['Campaign Return'])
     fig_campaign_return.update_layout(title_text='Campaign Return')
-    return fig_clv, fig_cpa, fig_clv_cpa, fig_new_customer, fig_campaign_ltv, fig_campaign_return
+
+    mean_new_customer=round(results_df['new_customer_count'].mean(),2)
+    mean_cpa=round(results_df['cpa'].mean(),2)
+    mean_diff=round(results_df['clv - cpa'].mean(),2)
+    mean_ltv=round(results_df['campaign_ltv'].mean(),2)
+    mean_return=round(results_df['campaign_return'].mean(),2)
+    
+    return fig_clv, fig_cpa, fig_clv_cpa, fig_new_customer,fig_campaign_ltv, fig_campaign_return, expected_clv,mean_cpa, mean_diff, mean_new_customer, mean_ltv, mean_return
 
 
 if __name__ == '__main__':
